093b6d499ee52fae521daf6ea86875507e051aba
Raaabbit <yangflo@foxmail.com>
2018-12-14 02:00:37 +0800

    微调搜索页面

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -9,7 +9,7 @@
         el-input.input-addr(v-model="addr" placeholder="酒店的具体名称(建议通过高德确认）")
         el-date-picker.input-date(v-model="startDate" type="date" placeholder="预测开始日期" value-format="yyyy-MM-dd" :editable="false")
         el-date-picker.input-date(v-model="endDate" type="date" placeholder="预测结束日期" value-format="yyyy-MM-dd" :editable="false")
-        el-button.input-btn(v-on:click="submit" type="primary" plain) 开始预测
+        el-button.input-btn(v-on:click="exactSearch" type="primary" plain) 开始预测
       form(v-show="this.searchType==1")
         el-input.input-addr(v-model="addr" placeholder="请输入要查询的范围（越精确越好哦）")
         el-date-picker.input-date(v-model="startDate" type="date" placeholder="预测开始日期" value-format="yyyy-MM-dd" :editable="false")
@@ -17,118 +17,165 @@
         //- 自定义的滑动轨道组件，来选择价格区间
         el-input.input-price(v-model="lowPrice" placeholder="价格下限（单位：￥）")
         el-input.input-price(v-model="highPrice" placeholder="价格上限（单位：￥）")
-        el-button.input-btn(v-on:click="submit" type="primary" plain) 开始预测
+        el-button.input-btn(v-on:click="rangeSearch" type="primary" plain) 开始预测
     .tip
       p.tip-text {{this.errorMsg}}
       img.tip-img(src="../assets/lazydog.png" title="请完整输入参数")
 </template>
 <script>
-import { Button, CollapseTransition } from 'element-ui'
-import ModelTab from '../components/ModelTab';
-import axios from 'axios';
+import { Button, CollapseTransition } from "element-ui";
+import ModelTab from "../components/ModelTab";
+import axios from "axios";
 export default {
   components: {
     ModelTab
   },
-  data(){
+  data() {
     return {
-      load:false,
-      searchType:0,
+      load: false,
+      searchType: 0,
       // 下面的一组数据是表单内容
-      addr:"",
-      startDate:"",
-      endDate:"",
-      lowPrice:"",
-      highPrice:"",
-      errorMsg:""
-    }
+      addr: "",
+      startDate: "",
+      endDate: "",
+      lowPrice: "",
+      highPrice: "",
+      errorMsg: ""
+    };
   },
-  mounted(){
+  mounted() {
     this.load = true;
   },
-  methods:{
-    getType(type){
+  methods: {
+    getType(type) {
       // 从model-tab接受类型之后，修改查找模式
       this.searchType = type;
     },
-    checkParam(){
+    checkParam() {
       // 检查参数输入
       if (this.searchType == 0) {
-        if (this.addr==""||this.startDate==""||this.endDate=="") {
+        if (this.addr == "" || this.startDate == "" || this.endDate == "") {
           return false;
         }
-      }else if (this.searchType == 1) {
-        this.lowPrice = this.lowPrice==""?this.lowPrice:0
-        this.highPrice = this.highPrice==""?this.highPrice:10000
-        if (this.addr==""||this.startDate==""||this.endDate=="") {
+      } else if (this.searchType == 1) {
+        this.lowPrice = this.lowPrice == "" ? this.lowPrice : 0;
+        this.highPrice = this.highPrice == "" ? this.highPrice : 10000;
+        if (this.addr == "" || this.startDate == "" || this.endDate == "") {
           return false;
         }
       }
       return true;
     },
-    submit(){
-      if(this.checkParam()){
-        this.noParam == false
-        // axios.get();
-      }else{
+    exactSearch() {
+      if (this.checkParam()) {
+        // 发起请求，查看用户要查找的酒店在不在数据库中
+        // 如果有，拿到酒店的id，并转到about
+
+        // 如果没有，提示用户，系统没有这家酒店的数据
+
+      } else {
         setTimeout(() => {
-          this.errorMsg = ""
-        }, 1000)
-        this.errorMsg = "请输入完整参数"
+          this.errorMsg = "";
+        }, 1000);
+        this.errorMsg = "请输入完整参数";
+      }
+    },
+    rangeSearch() {
+      if (this.checkParam()) {
+        // 先通过高德地图api，查找一定2000m内的所有酒店，将酒店名称整理成一个json发送
+        // 注意价格区间
+        // 拿到返回数据后，渲染酒店基本数据列表
+      } else {
+        setTimeout(() => {
+          this.errorMsg = "";
+        }, 1000);
+        this.errorMsg = "请输入完整参数";
       }
     }
   }
-}
+};
 </script>
 <style lang="stylus" scoped>
-.search 
-  position relative
-  padding 10px 
-  .underlayment 
-    position absolute
-    left 0
-    right 0
-    top 0
-    background-color #409EFF
-    z-index -1
-.search-parm
-  margin auto
-  width 90%
-  box-shadow 0 2px 12px 0 rgba(0,0,0,.1)
-  background-color #ffffff
-  border-radius 10px
-  padding 10px 10px
-  .tabs
-    width 100%
-    .tab 
-      width 50%
-.tip
-  .tip-text
-    width 100%
-    height 22px
-    text-align center
-    color var(--color-info)
-    font var(--size-normal)/1.1 var(--font-family)
-  .tip-img
-    display block
-    margin 20px auto
-@media only screen and (min-width: 320px ) 
-  .input-addr,.input-date,.input-btn
-    width 100%
-  .input-price
-    width 50%
-  .underlayment
-    height 200px  
-  .tip
-    width 80%
-    margin 10px auto 0
-    .tip-img
-      height 240px
-@media only screen and (min-width: 420px) 
-  .underlayment
-    height 200px 
-  .tip
-    width 80%
-    .tip-img
-      height 240px
+.search {
+  position: relative;
+  padding: 10px;
+
+  .underlayment {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    background-color: #409EFF;
+    z-index: -1;
+  }
+}
+
+.search-parm {
+  margin: auto;
+  width: 90%;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  background-color: #ffffff;
+  border-radius: 10px;
+  padding: 10px 10px;
+
+  .tabs {
+    width: 100%;
+
+    .tab {
+      width: 50%;
+    }
+  }
+}
+
+.tip {
+  .tip-text {
+    width: 100%;
+    height: 22px;
+    text-align: center;
+    color: var(--color-info);
+    font: var(--size-normal) / 1.1 var(--font-family);
+  }
+
+  .tip-img {
+    display: block;
+    margin: 20px auto;
+  }
+}
+
+@media only screen and (min-width: 320px) {
+  .input-addr, .input-date, .input-btn {
+    width: 100%;
+  }
+
+  .input-price {
+    width: 50%;
+  }
+
+  .underlayment {
+    height: 200px;
+  }
+
+  .tip {
+    width: 80%;
+    margin: 10px auto 0;
+
+    .tip-img {
+      height: 240px;
+    }
+  }
+}
+
+@media only screen and (min-width: 420px) {
+  .underlayment {
+    height: 200px;
+  }
+
+  .tip {
+    width: 80%;
+
+    .tip-img {
+      height: 240px;
+    }
+  }
+}
 </style>
