ab3662c2308dffc0da22adeb478a39deda736c9f
Raaabbit <yangflo@foxmail.com>
2018-12-14 02:00:21 +0800

    设计酒店基本信息卡片

---
+++ src/components/HotelCard.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .hotal-card
+    .photo
+      img(src="")
+    .msg
+      .name 酒店名称
+      .position 酒店位置
+      .advice 预计XX月XX日价格最低
+      .price 低至￥200
+      .detail-btn 查看详情
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="stylus">
+
+</style>
