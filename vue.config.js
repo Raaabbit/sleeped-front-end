4d7219f761ac899aad69f3b25844d2e5eef3c937
Raaabbit <yangflo@foxmail.com>
2018-12-21 19:21:31 +0800

    配置mock数据，修改history模式为hash

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,20 @@
-const mockdata = require('./mock/test.json');
+const errortip = require('./mock/error.json');
+const exactsearch = require('./mock/exactsearch.json');
+const rangesearch = require('./mock/rangesearch.json');
 
 module.exports={
+  baseUrl:'./',
   devServer: {
     port:4000,
     before(app){
-      app.get('/hotals/list',(req,res,next)=>{
-        res.json(mockdata);
+      app.get('/exactsearch',(req,res,next)=>{
+        res.json(exactsearch);
+      })
+      app.get('/rangesearch',(req,res,next)=>{
+        res.json(rangesearch);
+      })
+      app.get('/error',(req,res,next)=>{
+        res.json(errortip);
       })
     }
   }
