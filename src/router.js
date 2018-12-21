a51c6bfc96c0f245e0ea2910141420abbcdfc07e
Raaabbit <yangflo@foxmail.com>
2018-12-21 19:23:02 +0800

    嵌套路由about

--- src/router.js
+++ src/router.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Search from './views/Search.vue'
+import About from './views/About.vue';
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
@@ -17,7 +17,14 @@ export default new Router({
     {
       path: '/search',
       name: 'search',
-      component: Search
+      component: Search,
+      children: [
+        {
+          path: 'about',
+          name: 'about',
+          component: About
+        }
+      ]
     }
   ]
 })
