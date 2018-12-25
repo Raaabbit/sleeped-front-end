19766a1b0108a21646a46bce1f18b8693ae782dc
Raaabbit <yangflo@foxmail.com>
2018-12-25 12:02:55 +0800

    修改mock数据

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,17 @@
 const errortip = require('./mock/error.json');
 const exactsearch = require('./mock/exactsearch.json');
 const rangesearch = require('./mock/rangesearch.json');
+const prequery = require('./mock/test.json')
 
 module.exports={
   baseUrl:'./',
   devServer: {
     port:4000,
     before(app){
-      app.get('/exactsearch',(req,res,next)=>{
+      app.get('/prequery',(req,res,next)=>{
+        res.json(prequery)
+      })
+      app.post('/exactsearch',(req,res,next)=>{
         res.json(exactsearch);
       })
       app.get('/rangesearch',(req,res,next)=>{
